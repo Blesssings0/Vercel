e30f05addada3fa9799181621e570c2a667f1783
Blesssings0 <antoniojulio050@gmail.com>
2024-12-16 00:02:47 -0400

    More Authors

--- data/authors.js
+++ data/authors.js
@@ -8,7 +8,33 @@ const authors = [
     id: 2,
     name: "Gabriel García Márquez",
     nationality: "Colombian"
+  },
+  {
+    id: 3,
+    name: "Haruki Murakami",
+    nationality: "Japanese"
+  },
+  {
+    id: 4,
+    name: "J.K. Rowling",
+    nationality: "British"
+  },
+  {
+    id: 5,
+    name: "Chinua Achebe",
+    nationality: "Nigerian"
+  },
+  {
+    id: 6,
+    name: "Toni Morrison",
+    nationality: "American"
+  },
+  {
+    id: 7,
+    name: "Isabel Allende",
+    nationality: "Chilean"
   }
 ];
+
 
 module.exports = authors;
