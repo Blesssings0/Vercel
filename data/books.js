73357c93811b59e0116b81be24111fdc347c9a4d
Blesssings0 <antoniojulio050@gmail.com>
2024-12-16 00:25:37 -0400

    More_Books

--- data/books.js
+++ data/books.js
@@ -19,7 +19,57 @@ const books = [
     authorId: 2,
     year: 1967,
     available: true
+  },
+  {
+    id: 4,
+    title: "Norwegian Wood",
+    authorId: 3,
+    year: 1987,
+    available: true
+  },
+  {
+    id: 5,
+    title: "Kafka on the Shore",
+    authorId: 3,
+    year: 2002,
+    available: false
+  },
+  {
+    id: 6,
+    title: "Harry Potter and the Philosopher's Stone",
+    authorId: 4,
+    year: 1997,
+    available: true
+  },
+  {
+    id: 7,
+    title: "Harry Potter and the Chamber of Secrets",
+    authorId: 4,
+    year: 1998,
+    available: true
+  },
+  {
+    id: 8,
+    title: "Things Fall Apart",
+    authorId: 5,
+    year: 1958,
+    available: false
+  },
+  {
+    id: 9,
+    title: "Beloved",
+    authorId: 6,
+    year: 1987,
+    available: true
+  },
+  {
+    id: 10,
+    title: "The House of the Spirits",
+    authorId: 7,
+    year: 1982,
+    available: true
   }
 ];
+
 
 module.exports = books;
